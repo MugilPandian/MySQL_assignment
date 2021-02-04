7bc0489d457c3fccc83513a94f1f8bb60ceb13aa
Mugil Pandian V <MugilPandian@users.noreply.github.com>
2021-02-04 22:56:35 +0530

    node express body-parser mysql2 installed app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+var express= require('express');
+var app=express();
+var bodyParser =require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(__dirname));
+
+var route=require('./Controller/Routes/route'); 
+app.use(route);
+
+app.listen(3000,()=>{
+    console.log("Server is listening on port:3000");
+})
