369a2ad1136cfd2af03df36fdddba5f87188625e
Mugil Pandian V <MugilPandian@users.noreply.github.com>
2021-02-05 18:39:05 +0530

    Question 4 completed

--- Controller/routes/route.js
+++ Controller/routes/route.js
@@ -57,7 +57,7 @@ app.post('/question2insert',function(req,res){
         })
 })
 
-//QUESTION3
+//QUESTION THREE
 app.get('/question3',function(req,res){
     console.log("Question 3");
     res.sendFile('Question3.html',{root:'../MySQL_assignment/views'});
@@ -81,6 +81,51 @@ app.post('/question3insert',function(req,res){
             }
             else{
                 res.sendFile('Question3.html',{root:'../MySQL_assignment/views'});
+            }
+        })
+})
+
+//QUESTION FOUR
+app.get('/question4',function(req,res){
+    console.log("Question 4");
+    res.sendFile('Question4.html',{root:'../MySQL_assignment/views'});
+})
+
+app.post('/question4insert',function(req,res){
+    console.log('Question 4 inserted');
+    var Annualincome=req.body.AHI;
+    var Interest="";
+    if(req.body.sh!=undefined){
+        Interest+=req.body.sh+" ";
+    }
+    if(req.body.sp!=undefined){
+        Interest+=req.body.sp+" ";
+    }
+    if(req.body.tr!=undefined){
+        Interest+=req.body.tr+" ";
+    }
+    var Eye="";
+    if(req.body.red!=undefined){
+        Eye+=req.body.red;
+    }
+    if(req.body.green!=undefined){
+            Eye+=req.body.green+" ";
+    }
+    if(req.body.brown!=undefined){
+                Eye+=req.body.brown+" ";
+    }
+    if(req.body.blgold!=undefined){
+        Eye+=req.body.blgold+" ";
+
+    }
+        connection.query('insert into Misc_Information values(?,?,?)',[Annualincome,Interest,Eye],(err,results)=>{
+            if(err) throw err;
+            if(results){
+            console.log("Values Inserted");
+            res.sendFile('Valuesinserted.html',{root:'../MySQL_assignment/views'})
+            }
+            else{
+                res.sendFile('Question4.html',{root:'../MySQL_assignment/views'});
             }
         })
 })
